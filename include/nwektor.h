c30332edf2a45bdf41c5f199c8aa096fade5a6b5
Julia Zwierko <julia.z.by2005@gmail.com>
2024-03-23 15:03:39 +0100

    koncowe zmiany w nwektor.h - Julia

--- include/nwektor.h
+++ include/nwektor.h
@@ -6,73 +6,78 @@
 #include <utility>
 
 class nwektor {
-private:
-    double* elements;
-    size_t size;
-
 public:
-    // Zadanie 1: Konstruktor przyjmujący obiekt std::initializer_list<double>
+    // Konstruktor przyjmujący obiekt std::initializer_list<double>
     nwektor(std::initializer_list<double> values) : elements(new double[values.size()]), size(values.size()) {
         size_t i = 0;
         for (auto& value : values) {
             elements[i++] = value;
         }
     }
-
+    
     // Destruktor
     ~nwektor() {
         delete[] elements;
     }
 
-    // Operator dodawania (Zadanie 2)
+    // Konstruktor przenoszący
+    nwektor(nwektor&& other) noexcept : elements(nullptr), size(0) {
+        *this = std::move(other);
+    }
+
+    // Operator przypisania przenoszącego 
+    nwektor& operator=(nwektor&& other) noexcept {
+        if (this != &other) {
+            delete[] elements;
+            elements = other.elements;
+            size = other.size;
+            other.elements = nullptr;
+            other.size = 0;
+        }
+        return *this;
+    }
+
+    // Operator dodawania
     nwektor operator+(const nwektor& other) const {
         if (size != other.size) {
-            throw std::invalid_argument("Vectors have different sizes");
+            throw std::invalid_argument("Wektor ma inny rozmiar");
         }
         nwektor result{};
 
-        for (size_t i = 0; i < size; ++i) {
+        for (unsigned i = 0; i < size; i++) {
             result.elements[i] = elements[i] + other.elements[i];
         }
-
         return result;
     }
 
-    friend std::ostream& operator<<(std::ostream& os, const nwektor& vec) {
-        for (size_t i = 0; i < vec.size; ++i) {
-            os << vec.elements[i] << "\n";
+    // Operator przypisania
+    nwektor& operator=(const nwektor& other) {
+        if (this != &other) {
+            delete[] elements;
+            size = other.size;
+            elements = new double[size];
+            for (size_t i = 0; i < size; ++i) {
+                elements[i] = other.elements[i];
+            }
         }
+        return *this;
+    }
+
+    // Operator wypisywania 
+    friend std::ostream& operator<<(std::ostream& os, const nwektor& vec) {
+        os << "---Wypisanie wektora:---\n(";
+        for (size_t i = 0; i < vec.size; ++i) {
+            os << vec.elements[i] << " ";
+        }
+        os << ")";
         return os;
     }
-};
 
-// Zadanie 6: Klasa reprezentująca macierz n*n wymiarową
-class nmacierz {
 private:
-    nwektor* rows;
+    double* elements;
     size_t size;
-
-public:
-    
-    // Destruktor
-    ~nmacierz() {
-        delete[] rows;
-    }
-
-    // Operator dodawania (Zadanie 2)
-    nmacierz operator+(const nmacierz& other) const {
-        if (size != other.size) {
-            throw std::invalid_argument("Matrices have different sizes");
-        }
-
-        nmacierz result{};
-
-        for (size_t i = 0; i < size; ++i) {
-            result.rows[i] = rows[i] + other.rows[i];
-        }
-
-        return result;
-    }
- 
-
 };
+
+
+// TU ma byc klasa dla macierzy:
+// class ......
